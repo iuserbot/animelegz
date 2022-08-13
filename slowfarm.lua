93b2a0c75ec9957948e056c80ea73d890f7c5a49
iuserbot <101830604+iuserbot@users.noreply.github.com>
2022-08-13 21:13:55 +0300

    Update slowfarm.lua

--- slowfarm.lua
+++ slowfarm.lua
@@ -3,7 +3,10 @@ while wait() do
 while wait() do
 while wait() do
 while wait() do
-game:GetService("ReplicatedStorage").normalclick:FireServer(unpack({[1] = true}))
+while wait() do
+game:GetService("ReplicatedStorage").autoclick:FireServer()
+game:GetService("ReplicatedStorage").normalclick:FireServer(true)
+end
 end
 end
 end
