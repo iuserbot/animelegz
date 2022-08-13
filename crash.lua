8ad9ea386063d69fe1e919361cc90ca5a80b678f
iuserbot <101830604+iuserbot@users.noreply.github.com>
2022-08-13 10:59:54 +0300

    Update crash.lua

--- crash.lua
+++ crash.lua
@@ -30,6 +30,7 @@ while true do
 while true do
 while true do
 while true do
+--faruk koş ananı sikiyorlar --
 game:GetService("ReplicatedStorage").normalclick:FireServer(unpack({[1] = true}))
 end
 end
